4dd3c71d1a57355a9f90f3e218414a644c0ce569
SvGaming <sven.kovac1@outlook.com>
2024-10-10 00:18:21 +0200

    change about to id 8

--- cstats.py
+++ cstats.py
@@ -677,7 +677,7 @@ def main():
             print(c.aqua + "5) " + c.reset + "playerstats")
             print(c.aqua + "6) " + c.reset + "leaderboard")
             print(c.aqua + "7) " + c.reset + "capes")
-            print(c.aqua + "9) " + c.reset + "about")
+            print(c.aqua + "8) " + c.reset + "about")
             print(c.aqua + "0) " + c.reset + "exit")
 
             print("\nThis program is still a work in progress, report issues to SvGaming")
@@ -705,7 +705,7 @@ def main():
         elif choose == "7" or choose == "capes":
             cls()
             capes()
-        elif choose == "9" or choose == "about":
+        elif choose == "8" or choose == "about":
             cls()
             about()
         elif choose == "0" or choose == "exit":
